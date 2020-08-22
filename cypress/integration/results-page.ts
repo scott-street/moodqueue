3dcf2b5b81ea3982f24b9956eb5367ec739262de
Kate Brune <kbrune@iastate.edu>
2020-08-22 08:49:46 -0500

    cypress setup & circleci integration

---
+++ cypress/integration/results-page.ts
@@ -0,0 +1,10 @@
+export {} // error "All files must be modules when isolatedModules flag is provided"
+describe("Results page", () => {
+    beforeEach(() => {
+        cy.visit("/results")
+    })
+
+    it("should have the text Results Page", () => {
+        cy.contains("Results Page")
+    })
+})
