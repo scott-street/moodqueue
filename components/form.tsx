b18e344e885cff0102dc5362fb3e737aa0fa3765
Lorenzo Zenitsky <32914716+lorenzoz23@users.noreply.github.com>
2020-09-04 16:51:54 -0500

    added ui elements to form page (#14)
    * added ui elements to form page
    
    * removed spaced (for branding, ya know), add submit button back into the black box of death, added in responsiveness for mobile,
    changed radio buttons to checkboxes for reasons, and changed box header to an editable text input area with new queue as the placeholder
    
    * Update index-page.ts
    
    * okay, i swear it's done now
    
    * i hope ur mac feels better now

--- components/form.tsx
+++ components/form.tsx
@@ -1,124 +1,293 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import {
   Box,
   Heading,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Button,
+  Avatar,
+  Header,
   Text,
-  Avatar
+  RangeInput,
+  Button,
+  ResponsiveContext,
+  CheckBox,
+  TextInput
 } from 'grommet';
-import {
-  Favorite,
-  ShareOption,
-  Spotify,
-  User,
-  PlayFill,
-  SchedulePlay
-} from 'grommet-icons';
+import { Spotify, User, Subtract, Add, Emoji } from 'grommet-icons';
 import { UserInfo, defaultUser } from '../types/UserInfo';
+
+const icons = ['happy', 'euphoric', 'excited', 'sad', 'depressed', 'angry'];
 
 interface FormProps {
   user: UserInfo;
 }
 
-/**
- * the ui of this component should obviously be altered, i just quickly threw some components at the screen
- * @param props
- */
 const Form: FunctionComponent<FormProps> = (props) => {
-  const greeting =
-    'hello, ' + (props.user.name ? props.user.name.toLowerCase() : 'friend');
+  const [numSongs, setNumSongs] = useState(20);
+  const name = props.user.name ? props.user.name.toLowerCase() : 'stranger';
+
   return (
-    <Box align="center" justify="center">
-      <Box direction="row" align="center" gap="small">
-        <Heading textAlign="center" size="large">
-          {greeting}
-        </Heading>
-        {props.user.profileImages[0] ? (
-          <Avatar
-            src={props.user.profileImages[0].url}
-            size="xlarge"
-            onClick={() => window.open(props.user.profileUrl, '_blank')}
-            title="click to open your spotify profile"
-          />
-        ) : (
-          <Avatar
-            background="accent-2"
-            size="large"
-            onClick={() => window.open(props.user.profileUrl, '_blank')}
-            title="click to open your spotify profile"
+    <ResponsiveContext.Consumer>
+      {(size) => (
+        <Box fill justify="between">
+          <Header
+            justify={size !== 'small' ? 'evenly' : 'center'}
+            direction={size !== 'small' ? 'row' : 'column'}
           >
-            <User color="accent-1" size="large" />
-          </Avatar>
-        )}
-      </Box>
-      <Box direction="row" gap="large" align="center">
-        <Card
-          height="medium"
-          width="medium"
-          background="light-1"
-          align="center"
-          elevation="none"
-          border={{ side: 'all', color: 'accent-1', size: 'medium' }}
-        >
-          <CardHeader>
-            <Heading textAlign="center">playlist</Heading>
-          </CardHeader>
-          <CardBody align="center" justify="between" pad="small">
-            <Text textAlign="center">
-              create a playlist of songs based on a mood of your choosing
-            </Text>
-            <Button
-              label="continue"
-              alignSelf="center"
-              icon={<PlayFill />}
-              reverse
-              primary
-              color="neutral-3"
-            />
-          </CardBody>
-          <CardFooter background="light-3" fill="horizontal">
-            <Button icon={<Favorite color="red" />} hoverIndicator />
-            <Button icon={<ShareOption />} hoverIndicator />
-          </CardFooter>
-        </Card>
-        <Card
-          height="medium"
-          width="medium"
-          background="light-1"
-          align="center"
-          elevation="none"
-          border={{ side: 'all', color: 'accent-1', size: 'medium' }}
-        >
-          <CardHeader>
-            <Heading textAlign="center">queue</Heading>
-          </CardHeader>
-          <CardBody align="center" justify="between" pad="small">
-            <Text textAlign="center">
-              add select songs to your queue based on a mood of your choosing
-            </Text>
-            <Button
-              label="continue"
-              alignSelf="center"
-              icon={<SchedulePlay />}
-              reverse
-              primary
-            />
-          </CardBody>
-          <CardFooter fill="horizontal" background="light-3">
-            <Button icon={<Favorite color="red" />} hoverIndicator />
-            <Button icon={<ShareOption />} hoverIndicator />
-          </CardFooter>
-        </Card>
-      </Box>
-      <Heading textAlign="center" size="medium">
-        let your mood inspire you
-      </Heading>
-      <Spotify size="large" color="accent-1" />
-    </Box>
+            <Box border="between" gap="small">
+              <Heading
+                textAlign={size !== 'small' ? 'start' : 'center'}
+                size={size !== 'small' ? 'large' : 'medium'}
+                margin="none"
+              >
+                moodqueue
+              </Heading>
+              <Text
+                weight={size !== 'small' ? 'bold' : 'normal'}
+                textAlign={size !== 'small' ? 'start' : 'center'}
+                size={size !== 'small' ? 'large' : 'medium'}
+              >
+                let your mood inspire you
+              </Text>
+            </Box>
+            {size !== 'small' && (
+              <Box direction="row" align="center" gap="small">
+                <Heading textAlign="center" margin="none">
+                  {name}
+                </Heading>
+                {props.user.profileImages[0] ? (
+                  <Avatar
+                    src={props.user.profileImages[0].url}
+                    size="xlarge"
+                    border={{ size: 'small', side: 'all', color: 'accent-1' }}
+                    onClick={() => window.open(props.user.profileUrl, '_blank')}
+                    title="click to open your spotify profile"
+                  />
+                ) : (
+                  <Avatar
+                    background="accent-2"
+                    border={{ size: 'small', side: 'all', color: 'accent-1' }}
+                    size="large"
+                    onClick={() => window.open(props.user.profileUrl, '_blank')}
+                    title="click to open your spotify profile"
+                  >
+                    <User color="accent-1" size="large" />
+                  </Avatar>
+                )}
+              </Box>
+            )}
+          </Header>
+          <Box
+            align="center"
+            margin={size !== 'small' ? 'medium' : 'small'}
+            fill="vertical"
+            flex
+          >
+            <Box
+              fill={size === 'small'}
+              flex
+              justify="between"
+              align="center"
+              border={{
+                side: 'all',
+                size: 'xlarge',
+                style: 'outset',
+                color: 'accent-1'
+              }}
+              background={{ color: '#2F3E4D', opacity: 0.7 }}
+              round="large"
+              pad={{
+                horizontal: 'medium'
+              }}
+            >
+              <Box
+                justify="evenly"
+                align="center"
+                gap={size === 'small' ? 'large' : 'small'}
+              >
+                <TextInput
+                  title="click to edit"
+                  plain
+                  placeholder="new queue"
+                  style={{
+                    textAlign: 'center',
+                    fontSize:
+                      size === 'large' ? 48 : size === 'medium' ? 40 : 32
+                  }}
+                />
+                <Box gap="small" align="center">
+                  <Text
+                    textAlign="center"
+                    weight="bold"
+                    size={size !== 'small' ? 'medium' : 'small'}
+                  >
+                    your mood...
+                  </Text>
+                  <Box direction="row" gap="small">
+                    {icons.map((icon) => (
+                      <Box
+                        hoverIndicator="accent-1"
+                        round="small"
+                        pad={{ horizontal: 'small', vertical: 'xsmall' }}
+                        align="center"
+                        background="light-2"
+                        direction={size !== 'small' ? 'row' : 'column'}
+                        gap="xsmall"
+                        onClick={() => {}}
+                        key={icon}
+                      >
+                        {size !== 'small' && (
+                          <Text size="xsmall" weight="bold" textAlign="center">
+                            {icon}
+                          </Text>
+                        )}
+                        <Emoji color="brand" />
+                      </Box>
+                    ))}
+                  </Box>
+                </Box>
+                <Box gap="small" fill="horizontal">
+                  <Text
+                    textAlign="center"
+                    weight="bold"
+                    size={size !== 'small' ? 'medium' : 'small'}
+                  >
+                    number of songs:{' '}
+                    <Text
+                      textAlign="center"
+                      color="accent-1"
+                      size={size !== 'small' ? 'medium' : 'small'}
+                    >
+                      {numSongs}
+                    </Text>
+                  </Text>
+                  <Box direction="row" align="center" gap="small">
+                    <Button
+                      icon={
+                        <Subtract
+                          size={size !== 'small' ? 'medium' : 'small'}
+                        />
+                      }
+                      style={{ borderRadius: 30 }}
+                      onClick={() => {
+                        let num = numSongs;
+                        num--;
+                        setNumSongs(num);
+                      }}
+                    />
+                    <RangeInput
+                      max={50}
+                      min={1}
+                      step={1}
+                      name="number of songs:"
+                      value={numSongs}
+                      onChange={(event) => setNumSongs(+event.target.value)}
+                    />
+                    <Button
+                      icon={
+                        <Add size={size !== 'small' ? 'medium' : 'small'} />
+                      }
+                      style={{ borderRadius: 30 }}
+                      onClick={() => {
+                        let num = numSongs;
+                        num++;
+                        setNumSongs(num);
+                      }}
+                    />
+                  </Box>
+                </Box>
+                <Box
+                  direction="row"
+                  align="start"
+                  fill="horizontal"
+                  justify="evenly"
+                >
+                  <Text
+                    textAlign="center"
+                    weight="bold"
+                    size={size !== 'small' ? 'medium' : 'small'}
+                  >
+                    choose from your:
+                  </Text>
+                  <Box gap="small">
+                    <CheckBox
+                      label={
+                        <Box>
+                          <Text size={size !== 'small' ? 'medium' : 'small'}>
+                            saved songs
+                          </Text>
+                        </Box>
+                      }
+                    />
+                    <CheckBox
+                      label={
+                        <Box>
+                          <Text size={size !== 'small' ? 'medium' : 'small'}>
+                            top tracks
+                          </Text>
+                        </Box>
+                      }
+                    />
+                    <CheckBox
+                      label={
+                        <Box>
+                          <Text size={size !== 'small' ? 'medium' : 'small'}>
+                            top artists
+                          </Text>
+                        </Box>
+                      }
+                    />
+                    <CheckBox
+                      label={
+                        <Box>
+                          <Text size={size !== 'small' ? 'medium' : 'small'}>
+                            recommended
+                          </Text>
+                        </Box>
+                      }
+                    />
+                  </Box>
+                </Box>
+              </Box>
+
+              <Button
+                margin="small"
+                hoverIndicator={size !== 'small' ? 'accent-1' : false}
+                alignSelf="center"
+                primary={size === 'small'}
+                label="continue"
+                size={size === 'small' ? 'medium' : 'large'}
+                icon={<Spotify size={size !== 'large' ? 'medium' : 'large'} />}
+              />
+            </Box>
+          </Box>
+          {size === 'small' && (
+            <Box align="center" margin={{ bottom: 'small' }}>
+              {props.user.profileImages[0] ? (
+                <Avatar
+                  src={props.user.profileImages[0].url}
+                  size={size !== 'small' ? 'xlarge' : 'large'}
+                  border={{ size: 'small', side: 'all', color: 'accent-1' }}
+                  onClick={() => window.open(props.user.profileUrl, '_blank')}
+                  title="click to open your spotify profile"
+                />
+              ) : (
+                <Avatar
+                  background="accent-2"
+                  border={{ size: 'small', side: 'all', color: 'accent-1' }}
+                  size={size !== 'small' ? 'large' : 'medium'}
+                  onClick={() => window.open(props.user.profileUrl, '_blank')}
+                  title="click to open your spotify profile"
+                >
+                  <User
+                    color="accent-1"
+                    size={size !== 'small' ? 'large' : 'medium'}
+                  />
+                </Avatar>
+              )}
+            </Box>
+          )}
+        </Box>
+      )}
+    </ResponsiveContext.Consumer>
   );
 };
 
--- cypress/integration/index-page.ts
+++ cypress/integration/index-page.ts
@@ -4,7 +4,7 @@ describe('Index page', () => {
     cy.visit('/');
   });
 
-  it('should have the text mood queue', () => {
-    cy.contains('mood queue');
+  it('should have the text moodqueue', () => {
+    cy.contains('moodqueue');
   });
 });
