545f12611be9e2749ff9a9aef998d153bacb7272
Kate Brune <kbrune@iastate.edu>
2020-09-12 20:16:22 -0500

    form page reducer for state

--- __tests__/form.spec.tsx
+++ __tests__/form.spec.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Form from '../components/form';
-import { UserInfo } from '../types/UserInfo';
+import React from "react"
+import { shallow } from "enzyme"
+import Form from "../components/form/form"
+import { UserInfo } from "../types/UserInfo"
 
 const testUser: UserInfo = {
-  name: 'test',
-  id: 'test123',
-  email: 'test123@gmail.com',
-  profileUrl: 'spotify.com',
-  profileImages: [{ url: 'some-image.png' }]
-};
+    name: "test",
+    id: "test123",
+    email: "test123@gmail.com",
+    profileUrl: "spotify.com",
+    profileImages: [{ url: "some-image.png" }],
+}
 
-describe('Form', () => {
-  it('renders without crashing', () => {
-    shallow(<Form user={testUser} />);
-  });
-});
+describe("Form", () => {
+    it("renders without crashing", () => {
+        shallow(<Form user={testUser} />)
+    })
+})
