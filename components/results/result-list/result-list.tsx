482f74917f3e178e3a64e933337b8c9b908d21f9
Kate Brune <kbrune@iastate.edu>
2020-09-19 13:28:42 -0500

    results refactor/testing

---
+++ components/results/result-list/result-list.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react"
+import { Box, Heading, Text } from "grommet"
+import { Track } from "../../../types/Track"
+import { ResultListItem } from "../result-list-item/result-list-item"
+
+interface ResultListProps {
+    tracks: Track[]
+    dispatch: any
+    size: any
+}
+
+export const ResultList: FunctionComponent<ResultListProps> = (props) => {
+    const { tracks, dispatch, size } = props
+    return (
+        <Box
+            overflow={{ vertical: "auto" }}
+            gap="medium"
+            alignContent="center"
+            justify={tracks ? (tracks.length > 1 ? "start" : "center") : "center"}
+            pad={{
+                vertical: "small",
+                horizontal: size !== "small" ? "xlarge" : "none",
+            }}
+            fill
+        >
+            {tracks.map((track, i) => (
+                <ResultListItem key={i} track={track} size={size} dispatch={dispatch} />
+            ))}
+        </Box>
+    )
+}
