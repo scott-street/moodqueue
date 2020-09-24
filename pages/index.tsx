f7efcc1e3039a61972cb5ec2e3abe9071729bcb4
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-09-24 04:33:58 -0500

    updated manifest

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,6 +58,7 @@ const BaseApp: FunctionComponent = () => {
                                 name="viewport"
                                 content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
                             />
+                            <meta name="theme-color" content="#3f5efb" />
                             <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                             <link rel="shortcut icon" href="/favicon.ico" key={0} />
                             <link rel="manifest" href="/manifest.json" />
