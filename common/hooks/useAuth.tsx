998c1d1f4a79e3e2fcb46fc6b8a86e1e7c20f045
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-09-24 13:27:02 -0500

    have small screens opening up popup for spotify login instead of redirect

--- common/hooks/useAuth.tsx
+++ common/hooks/useAuth.tsx
@@ -45,13 +45,15 @@ export const AuthProvider: React.FunctionComponent<AuthProviderProps> = (props) 
         return text
     }
 
-    const openSpotifyAccountLogin = (redirect: string) => {
+    const openSpotifyAccountLogin = (redirect: string, size: string) => {
         const rand = generateRandomString(16)
         const scopes =
             "user-top-read user-library-read user-read-private user-read-email user-modify-playback-state"
         //make sure to change show_dialog to false if we don't want to show the spotify login redirect anymore
         const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=${scopes}&redirect_uri=${redirect}&state=${rand}&show_dialog=true`
-        window.location.href = url
+        if (size === "small") {
+            window.open(url, "_blank", "toolbar=0,location=0,menubar=0")
+        } else window.location.href = url
     }
     const setUserInfo = async () => {
         let newUser: UserInfo = {
