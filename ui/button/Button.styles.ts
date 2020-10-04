644dfdc981fc4f49a15fb8a5eb4ea81a96246246
Lorenzo Zenitsky <32914716+lorenzoz23@users.noreply.github.com>
2020-10-04 14:26:58 -0500

    added number selector to app, created storybook, made tests (#44)

--- ui/button/Button.styles.ts
+++ ui/button/Button.styles.ts
@@ -3,14 +3,14 @@ import { Button as GrommetButton } from "grommet"
 
 export const PrimaryButton = styled(GrommetButton)`
     background-color: #70ffaf;
-    border-radius: 20px;
+    border-radius: 30px;
     border: none;
     color: #666666;
 `
 
 export const SecondaryButton = styled(GrommetButton)`
     background-color: #81fced;
-    border-radius: 20px;
+    border-radius: 30px;
     border: none;
     color: #666666;
 `
---
+++ ui/number-selector/NumberSelector.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { Box as GrommetBox, RangeInput as NumberSelector } from "grommet"
+
+export const OuterBox = styled(GrommetBox)`
+    flex-direction: row;
+    display: flex;
+    align-items: center;
+`
+
+export const SizePicker = styled(NumberSelector)``
