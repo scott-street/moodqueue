43eda2c108de71f88f200d9ceb4f24a041dd4f46
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-10-07 04:38:20 -0500

    fixed logo

--- ui/logo/Logo.tsx
+++ ui/logo/Logo.tsx
@@ -14,20 +14,25 @@ export const Logo: React.FunctionComponent<LogoProps> = (props) => {
     const { id, size, textAlign, margin, header } = props
 
     return (
-        <LogoHeader id={id} size={header ? "medium" : size} textAlign={textAlign} margin={margin}>
+        <LogoHeader
+            id={id}
+            size={header ? "medium" : "large"}
+            textAlign={textAlign}
+            margin={margin}
+        >
             m
             <Happy
                 onClick={() => window.open("https://www.youtube.com/watch?v=cI0wUoCLnLk")}
                 style={{ cursor: "pointer" }}
-                width={size !== "small" ? (header ? "36px" : "48px") : "24px"}
-                height={size !== "small" ? (header ? "36px" : "48px") : "24px"}
+                width={size === "small" ? (header ? "24px" : "36px") : header ? "36px" : "48px"}
+                height={size === "small" ? (header ? "24px" : "36px") : header ? "36px" : "48px"}
                 id="happy-emoji"
             />
             <Sad
                 onClick={() => window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")}
                 style={{ cursor: "pointer" }}
-                width={size !== "small" ? (header ? "36px" : "48px") : "24px"}
-                height={size !== "small" ? (header ? "36px" : "48px") : "24px"}
+                width={size === "small" ? (header ? "24px" : "36px") : header ? "36px" : "48px"}
+                height={size === "small" ? (header ? "24px" : "36px") : header ? "36px" : "48px"}
                 id="sad-emoji"
             />
             dqueue
