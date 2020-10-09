767a8f514abd2b6c437eadfcba32b5abea42e09c
Lorenzo Zenitsky <lorenzoz@iastate.edu>
2020-10-09 02:57:27 -0500

    index file changes didnt work so reverting back

--- ui/backgrounds/index/IndexBackground.tsx
+++ ui/backgrounds/index/IndexBackground.tsx
@@ -12,8 +12,7 @@ export const Index: React.FunctionComponent = (props) => {
                         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
                     />
                     <meta name="theme-color" content="#3f5efb" />
-                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                    <meta name="mobile-web-app-capable" content="yes" />
+
                     <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
                     <link rel="shortcut icon" href="/favicon.ico" key={0} />
                     <link rel="manifest" href="/manifest.json" />
